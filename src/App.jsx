f86b265f2125a246d2b5763b321ea03de6e720d2
Gilberto da Silva <gilbertoantoniouk@gmail.com>
2023-12-15 17:21:50 +0000

    :lipstick:	feat: add global styles and gradient background

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,25 @@
+import styled from 'styled-components';
+import GlobalStyles from './components/GlobalStyles';
+
 function App() {
-  
+  const GradientBackground = styled.div`
+    background: linear-gradient(
+      175deg,
+      #120433 4.16%,
+      #04244f 48%,
+      #154580 96.76%
+    );
+    width: 100%;
+    min-height: 100vh;
+  `;
 
   return (
     <>
-      
+      <GradientBackground>
+        <GlobalStyles />
+      </GradientBackground>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
